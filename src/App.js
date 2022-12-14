c0a5e67a426f89096e3895c5437336133439e502
dangertater <106702944+dangertater@users.noreply.github.com>
2022-12-14 15:35:27 -0700

    added styled components to make sure mui wasnt fucking me up

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+import React from "react"
+import Header from "./Components/Header"
+import Container from "./Components/Styled/Container.Styled"
+import { ThemeProvider } from "@emotion/react"
+import { GlobalStyles } from "@mui/styled-engine"
+
+let theme = { colors: { header: "#ebfbff", body: "#fff", footer: "#003333" } }
+
 function App() {
-	return <></>
+	return (
+		<ThemeProvider theme={theme}>
+			<>
+				<GlobalStyles>
+					<Header></Header>
+					<Container></Container>
+				</GlobalStyles>
+			</>
+		</ThemeProvider>
+	)
 }
 
 export default App
